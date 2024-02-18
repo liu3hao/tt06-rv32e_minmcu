2fd428c333ea32c9a4757d7d7f2bd76b5fa268b0
Weihao <mail@weihao.dev>
2024-02-18 09:09:18 +0100

    1. test

--- src/project.v
+++ src/project.v
@@ -6,17 +6,17 @@
 `define default_netname none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  logic [7:0] ui_in,    // Dedicated inputs
+    output logic [7:0] uo_out,   // Dedicated outputs
+    input  logic [7:0] uio_in,   // IOs: Input path
+    output logic [7:0] uio_out,  // IOs: Output path
+    output logic [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  logic       ena,      // will go high when the design is enabled
+    input  logic       clk,      // clock
+    input  logic       rst_n     // reset_n - low to reset
 );
 
-    reg [7:0] counter;
+    logic [7:0] counter;
 
     always_ff @ (posedge clk, posedge rst_n) begin
         if (rst_n == 0) counter <= 0;
@@ -24,7 +24,7 @@ module tt_um_example (
     end
 
     assign uo_out = counter; 
-    assign uio_out = 0;
-    assign uio_oe = 0;
+    assign uio_out = 8'b10101010;
+    assign uio_oe =  8'b10101010;
 
 endmodule
